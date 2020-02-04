643da957e40fbb9656f6d07cd4f12a00bf67034f
mikesosa <michael.sosa@holbertonschool.com>
2020-02-04 11:33:57 -0500

    Updates

---
+++ precache-manifest.2990ba04c0659cab54e70ae2b42c1cb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df975b55a03a8bbfa99632aa9a8a91d0",
+    "url": "/CountrySuggestions/index.html"
+  },
+  {
+    "revision": "b97515ce46c8dff8483d",
+    "url": "/CountrySuggestions/static/css/main.1090deeb.chunk.css"
+  },
+  {
+    "revision": "6f7323af72c51240e979",
+    "url": "/CountrySuggestions/static/js/2.1deae109.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/CountrySuggestions/static/js/2.1deae109.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b97515ce46c8dff8483d",
+    "url": "/CountrySuggestions/static/js/main.01ecf116.chunk.js"
+  },
+  {
+    "revision": "dc8e52c8b9f536013b79",
+    "url": "/CountrySuggestions/static/js/runtime-main.d147e82c.js"
+  },
+  {
+    "revision": "1b3d637eb2bd9d4697f33f51483ff120",
+    "url": "/CountrySuggestions/static/media/bg-big.1b3d637e.jpg"
+  }
+]);
